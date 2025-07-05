2cd04a58861db6daa476b66ffa1b0e1a8a4da22c
Frank Jordan Villachica Gaspar <extfvillachica@belcorp.biz>
2025-07-04 19:53:22 -0500

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,15 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    },
-    output: "server",
+  vite: {
+      plugins: [tailwindcss()],
+  },
 
-    integrations: [react()],
+  output: "server",
+  integrations: [react()],
+  adapter: vercel(),
 });
